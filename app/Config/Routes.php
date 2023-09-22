cc27e8c0d61e99ef6b56547fe0418f2081935ef8
shifu013 <130575425+shifu013@users.noreply.github.com>
2023-09-22 12:11:50 +0800

    unknown

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('shifu', 'Home::shifu');
 $routes->get('test', 'MainController::test');
 $routes->post('save', 'MainController::save');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,8 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+    public function shifu()
+    {
+        echo 'hello';
+    }
 }
